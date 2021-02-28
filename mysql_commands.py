761d20548ff2d706f60b9558a2e7960f5671c3be
Harkeerat Singh <63100973+harkred@users.noreply.github.com>
2021-02-28 21:35:16 +0530

    Fixed bug in mysql_command.py so dat it can fetch results

--- mysql_commands.py
+++ mysql_commands.py
@@ -9,9 +9,9 @@ def execute_cmd(cmd, values=None, database=None):
     mydb = sql.connect(host=HOST, user=USER, passwd=PASSWD, database=database)
     curse = mydb.cursor()
     curse.execute(cmd, values)
-    mydb.commit()
-    mydb.close()
     try:
         result = curse.fetchall()
         return result
     except Exception as e: pass
+    mydb.commit()
+    mydb.close()
