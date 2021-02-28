a8e7d9ce03dd81a513fbd48e2f2726111fb2eb1d
Harkeerat Singh <63100973+harkred@users.noreply.github.com>
2021-02-28 21:37:19 +0530

    Added functionality for searching adn getting data for looking

--- mainpage_backend.py
+++ mainpage_backend.py
@@ -29,3 +29,28 @@ class MainpageBackend():
                 self.clear_entries(self.add_entries)
                 
             except Exception as e: msg.showerror('', 'Please fill the data appropriately')
+            
+    def search_record(self):
+        """For searching from the database"""
+        datas = execute_cmd("""
+                SELECT * FROM student_data WHERE {} = %s
+        """.format(self.bywat_combo.get()), values=(self.srch.get(),), database='school')
+        
+        self.srch.delete(0, tk.END)
+        
+        try: self.data_lst.delete(0, tk.END)
+        except Exception as e: pass
+        
+        if len(datas) == 0: msg.showerror('', 'Student data not found')
+        
+        else:
+            msg.showinfo('', 'Student data found')
+            
+            for data in datas:
+                self.data_lst.insert(tk.END, f'Id: {str(data[0])}')
+                self.data_lst.insert(tk.END, f'Name: {str(data[1])}')
+                self.data_lst.insert(tk.END, f'Class: {str(data[2])}')
+                self.data_lst.insert(tk.END, f'Phone: {str(data[3])}')
+                self.data_lst.insert(tk.END, f'DOB: {str(data[4])}')
+                self.data_lst.insert(tk.END, f'DOA: {str(data[5])}')
+                self.data_lst.insert(tk.END, '')
